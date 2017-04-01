3ac7c4df2cbceff8e4c4074202b7a0bde0596037
longyyuan <longyyuan@foxmail.com>
2017-04-02 03:36:52 +0800

    未完成暂存

--- det.c
+++ det.c
@@ -16,12 +16,12 @@ int **det;
 
 
 
-int get(int x, int y)
+int det_get(int x, int y)
 {
 	return det[x][y];
 }
 
-void set(int x, int y, int num)
+void det_set(int x, int y, int num)
 {
 	det[x][y] = num;
 }
@@ -31,9 +31,54 @@ int calc_inverse()
 {
 	int i,j;
 	
-	int *tmp = (int *)malloc(N * pow(N, N) * sizeof(int));
+	//列标序列组合结果（N × N种）
+	int list_size = pow(N, N);
+	
+	//列标序列组
+	int **c_list = (int **)malloc(list_size * sizeof(int *));
+	//int **r_list = (int **)malloc(list_size * sizeof(int *));
 
-	//for(i = 0;i < N;)
+	for(i = 0; i < list_size; i++)
+	{
+		//列标行标序列
+		c_list[i] = (int *)malloc(N * sizeof(int));
+		//r_list[i] = (int *)malloc(N * sizeof(int));
+
+	}
+	
+	//列标行标数组
+	int c[N];
+	//int r[N];
+	
+	//初始化
+	for(i = 0; i < N; i++)
+	{
+		c[i] = 0+1; 
+		//r[i] = i+1;
+	}
+	
+
+
+	for(i = 0; i < list_size; i++)
+	{
+		for(j = 0; j < N; j++)
+		{
+			c_list[i][j] = c[j];
+			//r_list[i][j] = r[j]; 
+		}
+	}
+
+
+
+
+	//销毁列标数组
+	for(i = 0; i < list_size; i++)
+	{
+		free(list[i]);
+	}
+
+	free(list);
+
 }
 
 int calc_det()
